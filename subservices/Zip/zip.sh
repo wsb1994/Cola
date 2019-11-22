a74a0a9c03e123db43351adfa46fd670d093be90
will b <willofhouse@pop-os.localdomain>
2019-11-21 19:05:04 -0500

    changes associated with zip files and moving zip files in a simple way with bash

--- subservices/Zip/zip.sh
+++ subservices/Zip/zip.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd ..
-pwd
 cd ..
-pwd
 cd Images
 cd $1
 
 zip -r $1.zip * $1
+find -type f -name '*png*' -delete
+find -type f -name '*jpeg*' -delete
+find -type f -name '*jpg*' -delete
+find -type f -name '*gif*' -delete
+
