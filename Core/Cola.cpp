27935ecd98d4e435c41b712fbf75794386e61480
willofhouse <willofhouse@pop-os.localdomain>
2019-12-04 09:28:47 -0500

    changes associated with preventing multiple instances of the same download, wasting resources on system. This is a concurrency issue. Cheating with Bash to make this quickly lead to a bug with multiple instances of the same download despite trying to use C to instantiate them rather than bash. Using a Set to prevent multiple runs was the fix.

--- Core/Cola.cpp
+++ Core/Cola.cpp
@@ -2,27 +2,41 @@
 #include <fstream>
 #include <unistd.h>
 #include <string>
+#include <set>
 using namespace std;
 int main(int argc, char **argv){
-
+	
+	//input file initialization
 	ifstream f;
 	f.open("subreddit.txt");
 	string _location = "";
 	string _command = "";
-while(!f.eof()){
-	//initialize to prevent weird C++ interactions
-	//this line prevents a bug where the code was reading twice. This is a hacky solution
-	//explore better options
-	f >> _location;
-	//lets get the command dynamically
-	//concurrency
-	_command = "python3 ../API/grab_pictures.py -s " + _location + " -loc ../Images &";
+	
+	
+	while(!f.eof()){
+	
 
-	system(_command.c_str());
-	system("clear");
+		//initialized above to prevent weird C++ interaction
+	
+		//these lines prevent a bug where the code was reading twice. This is a useful solution. Pretty sure this is linear
+		//and also easy to understand. Set
+		set<string> locales;
+	
+		f >> _location;
+	
+		//lets get the command dynamically
+		// with a little bit of concurrent scripting. This has diminishing returns
+		// with the number of processors you have available. may add a bash script that instead 
+		// runs one of two paths, concurrency or nonconcurrency based on the number of objects returned
+			if(locales.find(_location) == locales.end()){
+			
+				_command = "python3 ../API/grab_pictures.py -s " + _location + " -loc ../Images &";
+				system(_command.c_str());
+				locales.insert(_location);
+}	
 }
 
-f.close();
-system("clear");
-return 0;
+	f.close();
+	system("clear");
+	return 0;
 }
