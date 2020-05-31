d5c37eecc776ff421b0fd12299acf0d1f6104ee9
Mr. William Spicer <36477199+wsb1994@users.noreply.github.com>
2020-05-31 02:38:04 -0500

    comment

--- subservices/archive.sh
+++ subservices/archive.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+#Adjust directory and set datetime
 cd Database
 DIR=$(pwd)
 DT=$(date +%s)
 cd ..
 cd Zip
-
+#read from the images list
 IFS=$'\n' read -d '' -r -a lines < images.list
 cd ..
 cd ..
 cd Images
 
+#zip every image folder and add a date and time
 for var in "${lines[@]}"
 do
 	cd "${var}"
