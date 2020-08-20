8bb9286105b32ecf7506a31f4b6b9522cad03767
Mr. William Spicer <36477199+wsb1994@users.noreply.github.com>
2020-08-20 04:56:15 -0400

    Removing some nonsense that was left in as part of a demonstration

--- subservices/startup.sh
+++ subservices/startup.sh
@@ -5,20 +5,13 @@ apt-get install neofetch -y
 neofetch
 apt-get install at -y
 apt-get install gcc -y
-apt-get install spotify-client -y 
 apt-get install git -y
 apt-get install python3 -y
 apt-get install python3-pip -y
-snap install gotop -y
 apt-get install uuidgen -y
-
-apt-get install libreoffice -y
 
 pip3 install argparse
 pip3 install fake_useragent
-pip3 install pandas
-pip3 install tensorflow
-pip3 install python-twitter
 pip3 install python-opendb
 pip3 install certifi
 pip3 install chardet
